7de4aa4ffcf9694f26aac2496bddff0e163d44bb
Ivan Yanez <ivanyanez_sjc@hotmail.com>
2020-01-02 21:27:38 +0000

    Alteração das Classes departamento, funcionario e cargo controller , efetuado o mapeamento reconhecimento das pastas com os arquivos HTML,Css e Js

--- src/main/java/com/example/demo/web/controller/DepartamentoController.java
+++ src/main/java/com/example/demo/web/controller/DepartamentoController.java
@@ -1,5 +1,21 @@
 package com.example.demo.web.controller;
 
-public class DepartamentoController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("departamaento")
+public class DepartamentoController {
+	
+	@GetMapping("cadastrar")
+	public String cadastrar() {
+		return "/departamento/cadastro";
+	}
+
+	@GetMapping("listar")
+	public String Listar() {
+		return "departamento/lista";
+		
+	}
 }
--- src/main/java/com/example/demo/web/controller/FuncionarioController.java
+++ src/main/java/com/example/demo/web/controller/FuncionarioController.java
@@ -1,5 +1,25 @@
 package com.example.demo.web.controller;
 
-public class FuncionarioController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("funcionario")
+public class FuncionarioController {
+	
+	
+	@GetMapping("cadastrar")
+	public String cadastrar() {
+		
+		return "/funcionario/cadastro";
+		
+	}
+	@GetMapping("listar")
+	public String listar() {
+		
+		return "/funcionario/lista";
+		
+	}
+	
 }
