4608662288bbe8f01cc7335a26bca0c008a44226
Ivan Yanez <ivanyanez_sjc@hotmail.com>
2020-02-19 21:45:10 +0000

    finalizado Departamento Tela + Controler + Banco de DADOS

--- src/main/java/com/example/demo/web/controller/CargoController.java
+++ src/main/java/com/example/demo/web/controller/CargoController.java
@@ -1,13 +1,17 @@
 package com.example.demo.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.service.CargoService;
 
 @Controller
 @RequestMapping("/cargos")
 public class CargoController {
 
+	
 	@GetMapping("/cadastrar")
 	public String cadastrar(){
 		 return "/cargo/cadastro";
@@ -20,4 +24,9 @@ public class CargoController {
 		
 	}
 	
+	
+	public String salvar() {
+		return null;
+	
+	}
 }
--- src/main/java/com/example/demo/web/controller/DepartamentoController.java
+++ src/main/java/com/example/demo/web/controller/DepartamentoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.demo.service.DepartamentoService;
 import com.example.demo.web.domain.Departamento;
@@ -31,9 +32,10 @@ public class DepartamentoController {
 	}
 	
 	@PostMapping("/salvar")
-	public String salvar(Departamento departamento) {
+	public String salvar(Departamento departamento,RedirectAttributes attr) {
 		service.salvar(departamento);
-		
+		attr.addFlashAttribute("success", "Departamento inserido com sucesso");
+
 		return "redirect:/departamentos/cadastrar";
 		
 	}
@@ -48,8 +50,9 @@ public class DepartamentoController {
 	}
 	
 	@PostMapping("/editar")
-	public String editar(Departamento departamento) {
+	public String editar(Departamento departamento ,RedirectAttributes attr) {
 		service.editar(departamento);
+		attr.addFlashAttribute("success", "Departamento editado com sucesso");
 
 		return "redirect:/departamentos/cadastrar";
 
@@ -59,10 +62,15 @@ public class DepartamentoController {
 	@GetMapping("/excluir/{id}")
 	public String excluir(@PathVariable("id") Long id, ModelMap model) {
 	
-		if (!service.departamentoTemCargos(id)) {
+		if (service.departamentoTemCargos(id)) {
+			model.addAttribute("fail", "Departamento não removido. Possui cargos(s)vinculados(s)");
+			
+		}else {
 			service.excluir(id);
-		
-		} return "redirect:/departamentos/listar";
+			model.addAttribute("Success", "Departamento removido. com sucesso");
+
+		}
+		 return "redirect:/departamentos/listar";
 
 }
 
